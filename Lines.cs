cf87eef0b63055af97581a4a0816f78f545c5511
die lin <die lin@PEESEE>
2022-04-12 09:49:28 -0400

    .

--- Lines.cs
+++ Lines.cs
@@ -16,19 +16,18 @@ namespace StorybrewScripts
     {
         public override void Generate()
         {
-		    Rings(289488, 302730);
+		    Rings(289488, 302727);
         }
         public void Rings(int startTime, int endTime)
         {
             var blankBitmap = GetMapsetBitmap("sb/p.png");
-            var rotation = MathHelper.DegreesToRadians(90);
-            int amount = 18;
-            double angle = 45;
+            int amount = 20;
+            double angle = 90;
             double radius = 150;
             for (var i = 0; i < amount; i++)
             {
                 var Position = new Vector2(320, 240);
-                var ConnectionAngle = Math.PI / amount * 2;
+                double ConnectionAngle = Math.PI / amount;
 
                 Vector2 position = new Vector2(
                     (float)(320 + Math.Cos(angle) * radius),
@@ -40,22 +39,22 @@ namespace StorybrewScripts
                 lines.ScaleVec(startTime, 1, 20);
                 lines.Fade(startTime, startTime + 1000, 0, 0.15);
                 lines.Fade(startTime + 1000, endTime, 0.15, 0.8);
-                lines.Fade(endTime, endTime + 2000, 0.6, 0);
+                lines.Fade(endTime, endTime + 2000, 0.8, 0);
 
-                var timeStep = 67;
+                int timeStep = 61;
                 for (int time = startTime; time < endTime; time += timeStep)
                 {
-                    angle += -0.06;
+                    angle += -0.05;
                     radius += 0.01;
 
                     Vector2 nPosition = new Vector2(
                         (float)(320 + Math.Cos(angle) * radius),
                         (float)(240 + Math.Sin(angle) * radius));
 
-                    var Rotation = Math.Atan2((position.Y - nPosition.Y), (position.X - nPosition.X)) - Math.PI / 3;
+                    var Rotation = Math.Atan2((position.Y - nPosition.Y), (position.X - nPosition.X)) - Math.PI / 2;
 
-                    lines.Move(time, time + timeStep, Position, nPosition);
-                    lines.Rotate(time, time + timeStep, Rotation, Rotation);
+                    lines.Move(time, time + timeStep * 1.3, Position, nPosition);
+                    lines.Rotate(time, time + timeStep * 1.3, Rotation, Rotation);
 
                     Position = nPosition;
                 }
