40484839e4565045804cba683280a0d0cf7bc542
die lin <die lin@PEESEE>
2022-04-26 07:44:26 -0400

    added spectrum in glitch section

--- ScriptsManager.cs
+++ ScriptsManager.cs
@@ -18,6 +18,9 @@ namespace StorybrewScripts
         public override void Generate()
         {
             Rings(289489, 302730);
+
+            Spectrum(0, Color4.LightSteelBlue);
+            Spectrum(4, Color4.SteelBlue);
             //for overlappable objects
             FogGenerator fogManager = new FogGenerator(this);
 
@@ -81,7 +84,7 @@ namespace StorybrewScripts
         {
             Vector2 Position = new Vector2(250, 260);
             var Offset = new Vector2(Position.X, Position.Y);
-            int Rotation = 90;
+            int Rotation = 0;
 
             position = new Vector2(position.X - Offset.X, position.Y - Offset.Y);
             return Vector2.Transform(position, Quaternion.FromEulerAngles((float)(MathHelper.DegreesToRadians(Rotation)), 0, 0)) + Offset;
@@ -89,24 +92,24 @@ namespace StorybrewScripts
         public void Spectrum(float offset, Color4 Color)
         {
             var MinimalHeight = 0.1f;
-            Vector2 Scale = new Vector2(1, 200);
+            Vector2 Scale = new Vector2(1, 70);
             float LogScale = 600;
-            int Width = 500;
-            Vector2 Position = new Vector2(250, 260);
-            int Rotation = 90;
+            int Width = 250;
+            Vector2 Position = new Vector2(340, 360);
+            int Rotation = 0;
 
             int StartTime = 276247;
             int EndTime = 289488;
             int endTime = Math.Min(EndTime, (int)AudioDuration);
             int startTime = Math.Min(StartTime, endTime);
-            int BarCount = 20;
+            int BarCount = 10;
             var bitmap = GetMapsetBitmap("sb/p.png");
 
             var heightKeyframes = new KeyframedValue<float>[BarCount];
             for (var i = 0; i < BarCount; i++)
                 heightKeyframes[i] = new KeyframedValue<float>(null);
 
-            var fftTimeStep = Beatmap.GetTimingPointAt(startTime).BeatDuration / 8;
+            var fftTimeStep = Beatmap.GetTimingPointAt(startTime).BeatDuration / 11;
             var fftOffset = fftTimeStep * 0.2;
             for (var time = (double)startTime; time < endTime; time += fftTimeStep)
             {
@@ -132,18 +135,17 @@ namespace StorybrewScripts
                 var bar = GetLayer("Spectrum").CreateSprite("sb/p.png", OsbOrigin.Centre);
                 
                 bar.Color(startTime, Color);
-                bar.Fade(startTime, startTime + 500, 0, 0.8f);
-                bar.Fade(endTime, endTime + 1000, 0.8f, 0);
+                bar.Fade(endTime, endTime, 0.5f, 0);
 
                 bar.Rotate(OsbEasing.OutBack, startTime + BarCount * i, startTime + 1000 + BarCount * i,
-                    (float)(MathHelper.DegreesToRadians(Random(-50, 50))),
+                    (float)(MathHelper.DegreesToRadians(Rotation)),
                     (float)(MathHelper.DegreesToRadians(Rotation)));
 
                 bar.Move(OsbEasing.OutBack, startTime + BarCount * i, startTime + 1000 + BarCount * i,
-                    transform(new Vector2(-125 - Width + BarCount * i - offset, Random((int)Position.Y - 50, (int)Position.Y + 50) - offset)),
+                    transform(new Vector2(positionX - offset, Position.Y - offset)),
                     transform(new Vector2(positionX - offset, Position.Y - offset)));
 
-                var scaleX = Scale.X * barWidth / bitmap.Width;
+                var scaleX = Scale.X * barWidth / 2.5f / bitmap.Width;
                 scaleX = (float)Math.Floor(scaleX * 10) / 10.0f;
 
                 var hasScale = false;
