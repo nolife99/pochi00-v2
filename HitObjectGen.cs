7a5ea49c31121544c658f575de8324d081ab2f9f
die lin <die lin@PEESEE>
2022-05-06 09:09:03 -0400

    removed unnecessary assemblies

--- HitObjectGen.cs
+++ HitObjectGen.cs
@@ -4,7 +4,6 @@ using StorybrewCommon.Mapset;
 using StorybrewCommon.Scripting;
 using StorybrewCommon.Storyboarding;
 using StorybrewCommon.Storyboarding.Util;
-using StorybrewCommon.Subtitles;
 using StorybrewCommon.Util;
 using System;
 using System.Collections.Generic;
